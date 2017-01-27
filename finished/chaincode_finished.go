c79344d8102231bff4979d24cc846487088b2266
usama93yawar <usamayawar@gmail.com>
2017-01-27 20:02:38 +0500

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -97,13 +97,9 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 
 // read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp, user string
+	var key, jsonResp,  string
 	var err error
-	user, err := t.get_username(stub)
-	if (user != "user_type1_0"){
-	valAsbytes = "Permission denied"
-		return valAsbytes, nil
-	}
+	
 
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
